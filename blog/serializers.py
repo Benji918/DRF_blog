64af0a44b3b0b2dbd2b87fe8e9c9c323ff72f483
Benjamin <kodiugos@gmail.com>
2024-01-22 17:25:50 -0500

    file changes

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from core.models import BlogPost
 
 
-class BlogSerializer(serializers.Serializer):
+class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = ['id', 'title', 'content', 'author']
