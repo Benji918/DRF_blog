107ddbfc28625e0ab393cf9a82a64d0cc2507a57
Benjamin <kodiugos@gmail.com>
2024-01-22 15:48:21 -0500

    added CRUd blog functionality

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,2 +1,9 @@
 from rest_framework import serializers
 from core.models import BlogPost
+
+
+class BlogSerializer(serializers.Serializer):
+    class Meta:
+        model = BlogPost
+        fields = ['id', 'title', 'author']
+        read_only_fields = ['id']
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import BlogSerializer
+from core.models import BlogPost, CustomUser
+from rest_framework import viewsets, status
 
-# Create your views here.
+
+class BlogViewSets(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        """Return only Blog objects for the request user"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def perform_create(self, serializer):
+        """Create a new Blogs for specific authenticated user"""
+        return serializer.save(user=self.request.user)
