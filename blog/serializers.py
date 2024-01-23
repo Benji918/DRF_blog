0cc8c76f7b266cf3d2256a143e722c30ab802f02
Benjamin <kodiugos@gmail.com>
2024-01-23 11:31:49 -0500

    file changes

--- blog/serializers.py
+++ blog/serializers.py
@@ -5,5 +5,5 @@ from core.models import BlogPost
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
-        fields = ['id', 'title', 'content', 'author']
+        fields = ['id', 'title', 'content', 'created_at', 'updated_at']
         read_only_fields = ['id']
--- core/models.py
+++ core/models.py
@@ -38,9 +38,9 @@ class BlogPost(models.Model):
     )
     title = models.CharField(max_length=100, null=False, blank=False, unique=True)
     content = models.TextField(null=False, blank=False)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.author.username} ->{self.title}'
+        return f'{self.user.username} ->{self.title}'
